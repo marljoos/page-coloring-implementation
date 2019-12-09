639a06887eda99285a41dbd2049639f99803abe0
Marl Anthony Joos <m.joos@campus.tu-berlin.de>
2019-12-09 21:20:40 +0100

    Remove unnecessary imports.

--- page_coloring_model.py
+++ page_coloring_model.py
@@ -3,8 +3,7 @@
 
 import logging
 import sys
-from typing import List, Dict, Set, Tuple  # need this for specifying List/Dict/Set types for type hints
-from itertools import cycle
+from typing import List, Dict, Set  # need this for specifying List/Dict/Set types for type hints
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
